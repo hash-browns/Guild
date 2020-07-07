ca57448ff9941d01ba3cb170b75aad2eccfd5531
Joshua Brown <joshuabrown@Joshuas-MacBook.local>
2020-07-07 13:25:51 -0600

    fix tests

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,6 @@
 import unittest
 from dog_functions import foodOrder
-
+from dog_functions import overCapacity
 class NamesTestCase(unittest.TestCase):
 
     def test_acceptance(self):
@@ -9,16 +9,14 @@ class NamesTestCase(unittest.TestCase):
     def test_zero_dogs(self):
        self.assertEqual(foodOrder(0,0,0,0), 0)
        
-    #def test_negative_dogs(self)                       ##Covered in input validation dogs.py
-       #result = foodOrder(-1,-2,-3,-1)
-       #self.assertEqual(result, 0)
+    #def test_negative_dogs(self):                      ##Covered in input validation in dogs.py
+       #self.assertRaises(AssertionError,foodOrder,-1,1,1,1,)
 
-    #def test_letters(self):                            ##Covered in input validation in dogs.py
-       #self.assertRaises(ValueError, foodOrder('a',12,12,0))
+    def test_letters(self):                            
+       self.assertRaises(ValueError, foodOrder,'a',12,12,0)
 
     def test_greater_than_thirty(self):
-       result = foodOrder(12,12,12,0)
-       self.assertRaises(AssertionError)   
+       self.assertRaises(SystemExit,overCapacity,31,2,2)
     
     def test_max_dogs(self):
        self.assertEqual(foodOrder(30,0,0,17), 339.6)
