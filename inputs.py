7f527e71c76cfe44765e850843d7530f707206e9
hash-browns <52944361+hash-browns@users.noreply.github.com>
2020-07-07 00:14:30 -0600

    Create inputs.py

---
+++ inputs.py
@@ -0,0 +1,39 @@
+from guidsld import foodOrder
+from guidsld import overCapacity
+
+smallDogs = None
+while smallDogs == None:
+    try:
+        smallDogs = input("How many small dogs need food?")
+        if int(smallDogs) < 0:
+           print('Value cant be less than zero.')
+           smallDogs = None
+    except ValueError:
+        print('Numerical values only, please.')
+
+mediumDogs  = None
+while mediumDogs  == None:
+    try:
+        mediumDogs  = input("How many medium dogs need food?")
+        if int(mediumDogs) < 0:
+           print('Value cant be less than zero.')
+           mediumDogs  = None
+    except ValueError:
+        print('Numerical values only, please.')
+
+largeDogs  = None
+while largeDogs  == None:
+    try:
+        largeDogs  = input("How many large dogs need food?")
+        if int(largeDogs) < 0:
+           print('Value cant be less than zero.')
+           largeDogs  = None
+    except ValueError:
+        print('Numerical values only, please.')        
+
+overCapacity(largeDogs,mediumDogs,smallDogs)
+
+surplusFood = input("How much food was leftover from last month (in lbs)?")
+
+result = foodOrder(smallDogs,mediumDogs,largeDogs,surplusFood)
+print("Total lbs of food to order " + str(result))
